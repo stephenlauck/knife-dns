6860c0e3cddfc60d90f39b0d5b0997ef4d7bec48
Stephen Lauck <stpehen.lauck@gmail.com>
2012-12-12 15:23:39 -0800

    add zone list command

--- lib/chef/knife/dns_zone_list.rb
+++ lib/chef/knife/dns_zone_list.rb
@@ -18,24 +18,26 @@ require 'chef/knife/dns_base'
 
 class Chef
 	class Knife
-		class DnsDomainList < Knife
+		class DnsZoneList < Knife
 
 			include Knife::DnsBase
 
-			banner "knife dns domain list (options)"
+			banner "knife dns zone list (options)"
 
 			def run
 				domains = [
 				  ui.color('ID', :bold),
-				  ui.color('Domain', :bold)
+				  ui.color('Domain', :bold),
+ 				  ui.color('Nameservers', :bold)
 				]
 
 				self.connection.zones.sort_by(&:domain).each do |i|
 				  domains << i.id.to_s
 				  domains << i.domain
+				  domains << i.nameservers.join( ' ' )
 				end
 
-				puts ui.list(domains, :uneven_columns_across, 2)
+				puts ui.list(domains, :uneven_columns_across, 3)
 			end
 		end
 	end
