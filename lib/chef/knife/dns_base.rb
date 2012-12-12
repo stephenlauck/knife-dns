8a54a72750be7848e5d82a2e44579c5061aa8cc9
Stephen Lauck <stpehen.lauck@gmail.com>
2012-12-12 13:24:58 -0800

    add base for dns setup of connection

---
+++ lib/chef/knife/dns_base.rb
@@ -0,0 +1,77 @@
+#
+# Author:: Stephen Lauck (<stephen.lauck@gmail.com>)
+# Copyright:: Copyright (c) 2011-2012
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'chef/knife'
+
+class Chef
+  class Knife
+    module DnsBase
+
+      # :nodoc:
+      # Would prefer to do this in a rational way, but can't be done b/c of
+      # Mixlib::CLI's design :(
+      def self.included(includer)
+        includer.class_eval do
+
+          deps do
+            require 'fog'
+            require 'readline'
+            require 'chef/json_compat'
+          end
+
+          option :dns_provider,
+            :short => "-P PROVIDER",
+            :long => "--dns-provider PROVIDER",
+            :description => "Your DNS Provider",
+            :proc => Proc.new { |key| Chef::Config[:knife][:dns_provider] = key }
+
+
+          option :dns_email,
+            :short => "-A USERNAME",
+            :long => "--dns-username USERNAME",
+            :description => "Your DNS Username",
+            :proc => Proc.new { |key| Chef::Config[:knife][:dns_username] = key }
+
+          option :dns_password,
+            :short => "-K PASSWORD",
+            :long => "--dns-password PASSWORD",
+            :description => "Your DNS Password",
+            :proc => Proc.new { |key| Chef::Config[:knife][:dns_password] = key }
+        end
+      end
+
+      def connection
+        Chef::Log.debug("dns_username #{Chef::Config[:knife][:dns_username]}")
+        @connection ||= begin
+          connection = Fog::DNS.new(
+            :provider => Chef::Config[:knife][:dns_provider],
+            :dnsimple_email => Chef::Config[:knife][:dns_email],
+            :dnsimple_password => Chef::Config[:knife][:dns_password]
+          )
+        end
+      end
+
+      def msg_pair(label, value, color=:cyan)
+        if value && !value.to_s.empty?
+          puts "#{ui.color(label, color)}: #{value}"
+        end
+      end
+    end
+  end
+end
+
